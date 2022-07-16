82f4b786c9b188e7504ac929022420d95e4cb625
Zunawe <gyroscope15@gmail.com>
2022-07-16 09:47:45 -0700

    Add basic navmesh behavior to enemies

--- Assets/Scripts/Runtime/EnemyController.cs
+++ Assets/Scripts/Runtime/EnemyController.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
   [SerializeField] private DiePickupController _diePickupPrefab;
   [SerializeField] private int _health;
   [SerializeField] private int _contactDamage;
-  [SerializeField] private float _moveSpeed;
   private Rigidbody2D _rigidbody;
+  private NavMeshAgent _agent;
 
   void Start () {
     _rigidbody = GetComponent<Rigidbody2D>();
+    _agent = GetComponent<NavMeshAgent>();
+    _agent.updateRotation = false;
+    _agent.updateUpAxis = false;
   }
 
-  void FixedUpdate () {
-    _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, PlayerController.Instance.transform.position, _moveSpeed * Time.fixedDeltaTime));
+  void Update () {
+    _agent.destination = PlayerController.Instance.transform.position; 
   }
 
   private void OnCollisionEnter2D (Collision2D other) {
