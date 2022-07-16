88e599763456308400e5350ce0da5f7b7f4ebe25
Zunawe <gyroscope15@gmail.com>
2022-07-15 17:31:56 -0700

    Add audio tester for editor

--- Assets/Scripts/Runtime/AudioManager.cs
+++ Assets/Scripts/Runtime/AudioManager.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEngine.InputSystem;
+#endif
+
 public class AudioManager : MonoBehaviour {
   public static AudioManager Instance { get; private set; }
 
   [SerializeField] private GameObject _musicContainer;
   [SerializeField] private GameObject _sfxContainer;
+
+#if UNITY_EDITOR
+  public string TestSfxName;
+  public string TestMusicName;
+#endif
 
   private Dictionary<string, AudioSource> _music;
   private Dictionary<string, AudioSource> _sfx;
@@ -64,4 +73,18 @@ public class AudioManager : MonoBehaviour {
     _sfx[name].Play();
     _sfx[name].pitch = 1.0f;
   }
+
+#if UNITY_EDITOR
+  public void OnPlaySound (InputAction.CallbackContext context) {
+    if (context.performed) {
+      PlaySfx(TestSfxName);
+    }
+  }
+
+  public void OnPlayMusic (InputAction.CallbackContext context) {
+    if (context.performed) {
+      PlayMusicForceRestart(TestMusicName);
+    }
+  }
+#endif
 }
